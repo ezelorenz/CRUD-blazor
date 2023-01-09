83a42a86b26b9290ec3dc4709b41c3dca6fc50af
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-01-09 16:01:12 -0300

    Migracion desde base de datos

---
+++ BlazorCrud.API/Context/CrudblazorContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using BlazorCrud.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.API.Context;
+
+public partial class CrudblazorContext : DbContext
+{
+    public CrudblazorContext()
+    {
+    }
+
+    public CrudblazorContext(DbContextOptions<CrudblazorContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__Categori__8A3D240C23B2FB0E");
+
+            entity.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("descripcion");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("fechaRegistro");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__Producto__07F4A13289F9F27B");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.IdProducto).HasColumnName("idProducto");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("descripcion");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("marca");
+            entity.Property(e => e.Precio)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("precio");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK__Producto__idCate__276EDEB3");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BlazorCrud.API/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorCrud.API.Models;
+
+public partial class Categoria
+{
+    public int IdCategoria { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public virtual ICollection<Producto> Productos { get; } = new List<Producto>();
+}
